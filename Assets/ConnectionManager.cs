a2348a1d47095c410601d0ef2c414a82a27ab149
Ítalo <italo.ts4895@gmail.com>
2019-05-05 18:25:41 -0300

    first commit

---
+++ Assets/ConnectionManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ConnectionManager : NetworkBehaviour
+{
+    public GameObject canvasObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isLocalPlayer){
+            GameObject obj = InstantiateCanvas();
+            foreach(Transform t in obj.transform){
+                t.position += new Vector3(Random.Range(10f, 50f), 0, 0);
+            }
+        }
+    }
+
+    [Client]
+    GameObject InstantiateCanvas(){
+        Debug.Log("rodou");
+        return Instantiate(canvasObject);
+    }
+    
+    [TargetRpc]
+    public void TargetDestroyCanvas(NetworkConnection conn){
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("LocalPlayer")){
+            Destroy(go);
+        }
+    }
+}
---
+++ Assets/Scripts/CastellaNetManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CastellaNetManager : NetworkManager
+{
+    public override void Start(){
+        base.Start();
+    }
+
+    public override void OnServerConnect(NetworkConnection conn){
+        base.OnServerConnect(conn);
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn){
+        Debug.Log("Client disconected");
+        conn.playerController.GetComponent<ConnectionManager>().TargetDestroyCanvas(conn.playerController.connectionToClient);
+    }
+}
